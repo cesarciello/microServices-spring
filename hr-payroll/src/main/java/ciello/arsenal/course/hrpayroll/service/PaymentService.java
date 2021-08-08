6ed16b7facc4f4f6031086e6e9f37fc9d2419a49
Cesar Ciello <cesarciello15@gmail.com>
2021-08-08 18:45:28 -0300

    Feign client

--- hr-payroll/src/main/java/ciello/arsenal/course/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/ciello/arsenal/course/hrpayroll/service/PaymentService.java
@@ -1,12 +1,20 @@
 package ciello.arsenal.course.hrpayroll.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ciello.arsenal.course.hrpayroll.domain.Payment;
+import ciello.arsenal.course.hrpayroll.domain.Worker;
+import ciello.arsenal.course.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
+
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200d, days);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
