6dbd128281dc76ac6be49ad1fb82983269cc828d
Cesar Ciello <cesarciello15@gmail.com>
2021-08-09 21:07:48 -0300

    Timeout de Hystrix e Ribbon

--- hr-payroll/src/main/java/ciello/arsenal/course/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/ciello/arsenal/course/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import ciello.arsenal.course.hrpayroll.domain.Payment;
 import ciello.arsenal.course.hrpayroll.service.PaymentService;
 
@@ -17,9 +19,15 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value ="{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable int days) { 
 		Payment payment = paymentService.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, int days) {
+		Payment payment = new Payment("", 0d, days);
 		return ResponseEntity.ok(payment);
 	}
 }
